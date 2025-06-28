3530e252fbaa6ac13b662fab08c9818e0ad3a97e
GetOffer.help <13744916+getofferhelp@users.noreply.github.com>
2025-06-28 17:53:56 +0800

    Revert "a"
    This reverts commit b9050673c34bcf92ea459ba8ed25c79cbd4422f6.

--- src/types/analytics.d.ts
+++ src/types/analytics.d.ts
@@ -1,14 +1,9 @@
 // src/types/analytics.d.ts
-interface CfAnalytics {
-  trackEvent: (eventName: string, properties: object) => void
-}
-
 interface Window {
-  cfAnalytics?: CfAnalytics
-  gtag?: (
-    command: 'event',
-    eventName: string,
-    eventParameters: object
-  ) => void
-  _hmt?: any[]
-}
+    gtag?: (
+      command: 'event',
+      eventName: string,
+      eventParameters: object
+    ) => void
+    _hmt?: any[]
+  }
